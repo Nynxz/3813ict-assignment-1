c090393122929941c0f670ad541abd986c3c4b97
Henry Lee <contact@nynxz.com>
2024-08-13 21:58:23 +1000

    initial express setup

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const port = 3010;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
