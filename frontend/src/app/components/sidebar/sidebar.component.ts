55dc558d43c453e9fc1b15a9d194109f354fa7ec
Henry Lee <contact@nynxz.com>
2024-08-25 22:15:43 +1000

    update(frontend): update sidebar component

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { ButtonComponent } from '../ui/button/button.component';
 import { FoldButtonComponent } from '../ui/sidebar/fold-button/fold-button.component';
 import { NgClass } from '@angular/common';
+import {
+  PreferencesService,
+  UserPreferences,
+} from '../../services/preferences.service';
+import { LoginButtonComponent } from '../ui/sidebar/login-button/login-button.component';
+import { UserService } from '../../services/user.service';
+import { ServerWidgetComponent } from '../ui/sidebar/server-widget/server-widget.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ButtonComponent, FoldButtonComponent, NgClass],
+  imports: [
+    ButtonComponent,
+    FoldButtonComponent,
+    NgClass,
+    LoginButtonComponent,
+    ServerWidgetComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-  folded = true;
+  constructor(
+    private userPreferences: PreferencesService,
+    private userService: UserService
+  ) {}
+  folded = computed(() =>
+    this.userPreferences.preferences().sidebarfolded == 'true' ? true : false
+  );
+
   fold() {
-    console.log('folding?');
-    this.folded = !this.folded;
+    this.userPreferences.setItem(
+      'sidebarfolded',
+      this.folded() ? 'false' : 'true'
+    );
+  }
+
+  logout() {
+    this.userService.logout();
   }
 }
