848b39315881f62fd4d9382ad2d5eeafc124a226
Henry Lee <contact@nynxz.com>
2024-08-29 22:40:55 +1000

    refactor(frontend): add httpclient, rework core app component styling

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/login/login.component';
 import { UserComponent } from './routes/user/user.component';
+import { GroupComponent } from './routes/group/group.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'user', component: UserComponent },
+  { path: 'group', component: GroupComponent },
 ];
