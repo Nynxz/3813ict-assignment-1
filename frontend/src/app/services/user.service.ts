b580d38d66960027143905a4f814da8442ccb82d
Henry Lee <contact@nynxz.com>
2024-08-29 22:39:59 +1000

    feat(frontend): update user service to use JWT from backend

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,11 +1,26 @@
 import { Injectable, OnInit, signal, Signal } from '@angular/core';
 import { PreferencesService } from './preferences.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
+type LoginResponse = {
+  jwt: string;
+};
+type LoginJWT = {
+  username: string;
+  roles: string[];
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private preferencesService: PreferencesService) {
+  constructor(
+    private preferencesService: PreferencesService,
+    private httpClient: HttpClient,
+    private router: Router
+  ) {
     console.log('ON INIT USER');
     this.auto_login();
   }
@@ -17,20 +32,50 @@ export class UserService {
   name = signal('');
 
   auto_login() {
-    let _name = this.preferencesService.getItem('name');
-    if (_name) {
-      this.name.set(_name);
+    let _jwt = this.preferencesService.getItem('jwt');
+    if (_jwt) {
+      try {
+        this.name.set((jwtDecode(_jwt) as LoginJWT).username);
+        this.router.navigateByUrl('/user');
+      } catch (error) {
+        console.log(error);
+      }
     }
-    console.log(_name);
   }
 
-  login(_name: string) {
-    this.name.set(_name);
-    this.preferencesService.setItem('name', '_name');
+  login(email: string, password: string) {
+    this._backendLogin(email, password).subscribe((e) => {
+      this.preferencesService.setItem('jwt', e.jwt);
+      console.log(e.jwt);
+      this.auto_login();
+    });
+    // this.name.set(_name);
+  }
+
+  _backendLogin(email: string, password: string) {
+    return this.httpClient
+      .post<LoginResponse>(
+        'http://localhost:3010/login',
+        JSON.stringify({ email, password }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          return throwError(
+            () => new Error('Something bad happened; please try again later.')
+          );
+        })
+      );
   }
 
   logout() {
     this.name.set('');
     this.preferencesService.setItem('name', '');
+    this.preferencesService.setItem('jwt', '');
+    this.router.navigateByUrl('/login');
   }
 }
