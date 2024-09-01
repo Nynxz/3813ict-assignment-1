8c70b0dedbf6b033a52a7efa259ee0483b71c0b7
Henry Lee <contact@nynxz.com>
2024-09-01 15:54:33 +1000

    feat(frontend): add 404 page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/login/login.component';
 import { UserComponent } from './routes/user/user.component';
 import { GroupComponent } from './routes/group/group.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'user', component: UserComponent },
-  { path: 'group', component: GroupComponent },
+  { path: 'group/:id/:channel', component: GroupComponent },
+  { path: 'group/:id', component: GroupComponent },
+  { path: '**', component: NotfoundComponent },
 ];
