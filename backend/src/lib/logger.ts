6ffc6fd104052655cc9231d91595ca72ab602ac0
Henry Lee <contact@nynxz.com>
2024-09-02 07:03:10 +1000

    feat(backend): new debug print colours

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,3 +1,5 @@
+import { debugCurrentDate } from "./utils";
+
 // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 export class Logger {
   static logGreen(text: string) {
@@ -8,6 +10,15 @@ export class Logger {
   }
   static logRed(text: string) {
     console.error("[gateway]: " + text);
+  }
+  static logDebugGreen(text: string) {
+    console.log(`\x1b[32m[DEBUG]: ${debugCurrentDate(text)}\x1b[0m`);
+  }
+  static logDebugOrange(text: string) {
+    console.warn(`[DEBUG]: ${debugCurrentDate(text)}`);
+  }
+  static logDebugRed(text: string) {
+    console.error(`[DEBUG]: ${debugCurrentDate(text)}`);
   }
   static logOrange(text: string) {
     console.warn("[gateway]: " + text);
