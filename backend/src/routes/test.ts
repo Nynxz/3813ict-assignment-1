fdc775ddf6b80388489bb454b4d08903612bf246
Henry Lee <contact@nynxz.com>
2024-09-02 07:05:32 +1000

    refactor(backend): cleanup route files into seperate files for (group, user)

--- backend/src/routes/test.ts
+++ backend/src/routes/test.ts
@@ -1,48 +1,13 @@
 import { Request, Response, Router } from "express";
 import { registerHTTP } from "../lib/registerHTTP";
-import { Gateway } from "../gateway";
-import { createServer, findServers, updateServer } from "../lib/db";
-import { MongoClient } from "mongodb";
-import { verify } from "jsonwebtoken";
-import { Roles } from "./login";
+import { Gateway, GRouter } from "../gateway";
 import requireValidRole from "../middleware/requireValidRole";
 import requireBodyKey from "../middleware/requireBodyKey";
+import { Roles } from "../db/user";
 
-// TODO: Move server shit out of test.ts into actual 'server.ts' file
-export default (router: Router, gateway: Gateway) => {
+export default (router: GRouter, gateway: Gateway) => {
   registerHTTP("get", "/", router, (req, res) => {
     res.send("Connected to Backend");
-  });
-
-  registerHTTP("get", "/servers", router, async (req, res) => {
-    // TODO, take JWT, give servers accessible to user of JWT, or P.O
-    res.send(await findServers(gateway.db as MongoClient));
-  });
-
-  registerHTTP("post", "/createserver", router, async (req, res) => {
-    // TODO: anyone can create a server right now,
-    // setup JWT middleware or something, 'SuperONLY > AdminONLY > UserONLY`
-    const server = await createServer(
-      gateway.db as MongoClient,
-      req.body.serverName as String
-    );
-    res.send("Complete " + JSON.stringify(server));
-  });
-
-  registerHTTP("post", "/server/update", router, async (req, res) => {
-    console.log("GOT REQUEST");
-    console.log(req.body);
-    //TODO: averygoodaverynice, but this IS NOT ROLE RESTRICTED
-    try {
-      const decodedJWT = verify(req.body.jwt, "thisisasecret:):)");
-      console.log(decodedJWT);
-      const server = await updateServer(gateway.db as MongoClient, req.body);
-      res.send(server);
-    } catch (error) {
-      console.log(error);
-      res.statusCode = 401;
-      res.send(JSON.stringify({ error: "invalid JWT" }));
-    }
   });
 
   //TODO: table tennis kinda bad, to remove
