871f2eb0fb3689561c2caaf6495ea3413ded5233
Henry Lee <contact@nynxz.com>
2024-09-02 18:02:34 +1000

    refactor(frontend): cleanup login-form

--- frontend/src/app/components/forms/login-form/login-form.component.ts
+++ frontend/src/app/components/forms/login-form/login-form.component.ts
@@ -1,3 +1,4 @@
+import { Error } from '@/error.class';
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -13,48 +14,22 @@ import { UserService } from '@services/user/user.service';
 export class LoginFormComponent {
   email = '';
   password = '';
-  error = false;
+  Error = new Error();
+
   constructor(
     private userService: UserService // private helloWorldService: HelloworldService, // private preferencesService: PreferencesService, // private loginService: LoginService, // private userService: UserService, // private router: Router
   ) {}
 
   handleSubmit() {
-    // submit to backend :) get jwt, store jwt, ez pz
-    console.log(this.email, this.password);
     if (this.email == '' || this.password == '') {
-      this.error = true;
+      this.Error.set('Empty Input');
       return;
+    } else {
+      this.userService.login(this.email, this.password).then((e: any) => {
+        if (e.error) {
+          this.Error.set(e.error);
+        }
+      });
     }
-    this.userService.login(this.email, this.password);
-    // this.loginService.login(this.email, this.password).subscribe({
-    //   next: (e) => {
-    //     console.log(e);
-    //     let jwt: LoginResponse = e as LoginResponse;
-    //     console.log(e);
-    //     this.preferencesService.setItem('jwt', jwt.jwt);
-    //     let jwtDecoded = this.loginService.decodeJWT(jwt.jwt) as {
-    //       username: string;
-    //     };
-    //     console.log(jwtDecoded);
-    //     this.userService.setUserInfo({
-    //       username: jwtDecoded.username,
-    //       isLoggedIn: true,
-    //     });
-    //     this.loginService.setDetails(jwtDecoded.username);
-    //     this.userService.setIsLoggedInBoolean(true);
-    //     this.helloWorldService.debug.next('DEBUG: YES!');
-    //     this.router.navigateByUrl('/user');
-    //   },
-    //   error: (e) => {
-    //     console.log(e);
-    //     this.error = true;
-    //   },
-    // });
-    // window.location.reload();
-  }
-
-  logout() {
-    // this.loginService.logout();
-    // window.location.reload();
   }
 }
